9cdd65848cc8bcf108235a9e2f955f8a21652b24
amirjamil90 <amirjamil900@gmail.com>
2019-09-23 19:35:51 +0300

    Fixed Pandas Read from Excel file for coordinates

---
+++ import_nation.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+df = pd.read_excel('nation.xlsx', sheet_name='Sheet1',sep='\s*,\s*')
+
+print("Column headings:")
+a=df['LATITUDE'].tolist()
+b=df['LONGITUDE'].tolist()
+
+for i in range(0,len(a)):
+	print(a[i])
+	print(b[i])
+
--- nationaladdress.py
+++ nationaladdress.py
@@ -1,31 +1,40 @@
 import httplib, urllib, base64
 import json
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import time
+df = pd.read_excel('nation.xlsx', sheet_name='Sheet1',sep='\s*,\s*')
+latitudes=df['LATITUDE'].tolist()
+longitudes=df['LONGITUDE'].tolist()
+for i in range(0,len(latitudes)):
 
-headers = {
-    # Request headers
-    'api_key': 'PRIMARY API KEY',
-}
+    headers = {
+        # Request headers
+        'api_key': 'primary key',
+    }
 
-params = urllib.urlencode({
-    # Request parameters
-    'lat': 'LATITUDE',
-    'long': 'LONGITUDE',
-    'language': 'A',
-    'format': 'json',
-    'encode': 'utf8',   #THIS IS VERY IMPORTANT BECAUSE SOME SYMBOLOGY NOT DEFINED IN LANGAUGE SET. 
-})
+    params = urllib.urlencode({
+        # Request parameters
+        'lat': latitudes[i],
+        'long': longitudes[i],
+        'language': 'A',
+        'format': 'json',
+        'encode': 'utf8',   #THIS IS VERY IMPORTANT BECAUSE SOME SYMBOLOGY NOT DEFINED IN LANGAUGE SET. 
+    })
 
-try:
-    conn = httplib.HTTPSConnection('apina.address.gov.sa')
-    #REQUEST PARAMETERS
-    conn.request("GET", "/NationalAddress/v3.1/Address/address-geocode?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    json_data=json.loads(data)
-    print(json_data['Addresses'][0]['PKAddressID'])     #TO GET THE NATIONAL ADDRESS COMPLETE CODE WHICH INCLUDES POSTAL CODE, BULDING CODE AND ADDITIONAL CODE. 
-    conn.close().    #CLOSE THE CONNECTION. WE NEED TO CLOSE THE CONNECTION EVERYTIME. SO FOR MULTIPLE REQUEST MULTIPLE CONNECTIONS NEEDED AS THREAD. 
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    try:
+        conn = httplib.HTTPSConnection('apina.address.gov.sa')
+        #REQUEST PARAMETERS
+        conn.request("GET", "/NationalAddress/v3.1/Address/address-geocode?%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        json_data=json.loads(data)
+        print(json_data['Addresses'][0]['PKAddressID'])     #TO GET THE NATIONAL ADDRESS COMPLETE CODE WHICH INCLUDES POSTAL CODE, BULDING CODE AND ADDITIONAL CODE. 
+        conn.close()  
+        time.sleep(4)              #CLOSE THE CONNECTION. WE NEED TO CLOSE THE CONNECTION EVERYTIME. SO FOR MULTIPLE REQUEST MULTIPLE CONNECTIONS NEEDED AS THREAD. 
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 
 
